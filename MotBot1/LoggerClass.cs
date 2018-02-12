4a485d8209890fe62460e2dfc1a8e97991c7eae8
MixMasterMot <ljbotha93@gmail.com>
2018-02-12 17:42:43 +0200

    Add files via upload

---
+++ MotBot1/LoggerClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace MotBot1
+{
+    public class LoggerClass
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void LogError(string msg)
+        {
+            logger.Error("Error message", msg);
+        }
+
+        public static void LogInfo(string msg)
+        {
+            logger.Info("Info", msg);
+        }
+    }
+}
---
+++ MotBot1/Modules/Ping.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotBot1.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("Reply")]
+        public async Task PingAsync([RemainderAttribute]string reply="Blyat")
+        {
+            await ReplyAsync(reply);
+        }
+
+        [Command("hang")]
+        public async Task HangManStart(string ltr ="new")
+        {
+            if (ltr == "new")
+            {
+                await ReplyAsync("Hang the hangman", false, HangMan.NewGame());
+                return;
+            }
+            else if (ltr.Length > 1)
+            {
+                await ReplyAsync("Only one leter at a time");
+            }
+            else
+            {
+                Tuple<string, Embed> tpl = HangMan.HangManWordContains(ltr);
+                if (tpl.Item1 != "")
+                {
+                    await ReplyAsync(tpl.Item1);
+                }
+                else
+                {
+                    await ReplyAsync("", false, tpl.Item2);
+                }
+            }
+          
+        }
+
+        [Command("help")]
+        public async Task GetHelp()
+        {
+            await ReplyAsync("", false, Help.Helpper());
+        }
+    }
+}
